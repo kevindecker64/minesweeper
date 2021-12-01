607006923056e22bda5e0123e2bc09f5cf351cc2
Kevin Decker <KevinDecker64@gmail.com>
2021-12-01 17:17:57 -0600

    checks empty squares for adjacent bombs

--- js/main.js
+++ js/main.js
@@ -16,14 +16,74 @@ function createBoard() {
   const emptyArray = Array(width * width - bombCount).fill("valid");
   const gameArray = emptyArray.concat(bombsArray);
   const shuffledArray = gameArray.sort(() => Math.random() - 0.5);
-
-
+  // fills board with bombs
   for (let i = 0; i < width * width; i++) {
     const square = document.createElement("div");
     square.setAttribute("id", i);
-    square.classList.add(shuffledArray[i])
+    square.classList.add(shuffledArray[i]);
     grid.appendChild(square);
     squares.push(square);
+  }
+  // checks for bombs next to empty squares
+  for (let i = 0; i < squares.length; i++) {
+    const leftEdge = i % width === 0;
+    const rightEdge = i % width === width - 1;
+    const topEdge = i < width;
+    const bottomEdge = i > width * width - (width + 1);
+    let total = 0;
+    if (squares[i].classList.contains("valid")) {
+      // check Left
+      if (!leftEdge && squares[i - 1].classList.contains("bomb")) {
+        total++;
+      }
+      // check UpLeft
+      if (
+        !topEdge &&
+        !leftEdge &&
+        squares[i - 1 - width].classList.contains("bomb")
+      ) {
+        total++;
+      }
+      // check Up
+      if (!topEdge && squares[i - width].classList.contains("bomb")) {
+        total++;
+      }
+      // check UpRight
+      if (
+        !topEdge &&
+        !rightEdge &&
+        squares[i + 1 - width].classList.contains("bomb")
+      ) {
+        total++;
+      }
+      // check Right
+      if (!rightEdge && squares[i + 1].classList.contains("bomb")) {
+        total++;
+      }
+      // check DownRight
+      if (
+        !bottomEdge &&
+        !rightEdge &&
+        squares[i + 1 + width].classList.contains("bomb")
+      ) {
+        total++;
+      }
+      // check Down
+      if (!bottomEdge && squares[i + width].classList.contains("bomb")) {
+        total++;
+      }
+      // check DownLeft
+      if (
+        !bottomEdge &&
+        !leftEdge &&
+        squares[i - 1 + width].classList.contains("bomb")
+      ) {
+        total++;
+      }
+      // add total adjacent bombs to square
+      squares[i].setAttribute("data", total);
+      console.log(squares[i]);
+    }
   }
 }
 
