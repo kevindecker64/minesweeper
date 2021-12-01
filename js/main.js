dde8d4f4ba3f776f41e3e4f983bba3a5715fc69c
Kevin Decker <KevinDecker64@gmail.com>
2021-12-01 14:35:34 -0600

    randomly fills grid with 20 bombs

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@
 const grid = document.querySelector(".grid");
 
 /*-- Variables --*/
-let width = 10;
 let squares = [];
+let width = 10;
+let bombCount = 20;
 
 /*-- Cached Elements --*/
 
@@ -11,9 +12,16 @@ let squares = [];
 
 /*-- Functions --*/
 function createBoard() {
+  const bombsArray = Array(bombCount).fill("bomb");
+  const emptyArray = Array(width * width - bombCount).fill("valid");
+  const gameArray = emptyArray.concat(bombsArray);
+  const shuffledArray = gameArray.sort(() => Math.random() - 0.5);
+
+
   for (let i = 0; i < width * width; i++) {
     const square = document.createElement("div");
     square.setAttribute("id", i);
+    square.classList.add(shuffledArray[i])
     grid.appendChild(square);
     squares.push(square);
   }
