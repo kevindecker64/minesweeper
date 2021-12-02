6ec444a48802b52fc221c2ad4e5b0244e60966ba
Kevin Decker <KevinDecker64@gmail.com>
2021-12-02 14:18:32 -0600

    adds primary click function with recursive checks on adjacent squares

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 const grid = document.querySelector(".grid");
 
 /*-- Variables --*/
+let playable = true;
 let squares = [];
 let width = 10;
 let bombCount = 20;
@@ -23,6 +24,10 @@ function createBoard() {
     square.classList.add(shuffledArray[i]);
     grid.appendChild(square);
     squares.push(square);
+
+    square.addEventListener("click", function (e) {
+      click(square);
+    });
   }
   // checks for bombs next to empty squares
   for (let i = 0; i < squares.length; i++) {
@@ -82,9 +87,84 @@ function createBoard() {
       }
       // add total adjacent bombs to square
       squares[i].setAttribute("data", total);
-      console.log(squares[i]);
     }
   }
+}
+
+function click(square) {
+  let currentId = square.id;
+  if (!playable) return;
+  if (square.classList.contains("checked") || square.classList.contains("flag"))
+    return;
+  if (square.classList.contains("bomb")) {
+    console.log("Game Over!");
+  } else {
+    let total = square.getAttribute("data");
+    if (total != 0) {
+      square.classList.add("checked");
+      square.innerHTML = total;
+      return;
+    }
+    checkSquare(square, currentId);
+  }
+  square.classList.add("checked");
+}
+
+function checkSquare(square, currentId) {
+  const leftEdge = currentId % width === 0;
+  const rightEdge = currentId % width === width - 1;
+  const topEdge = currentId < width;
+  const bottomEdge = currentId > width * width - (width + 1);
+  setTimeout(() => {
+    // check Left
+    if (!leftEdge) {
+      const newId = parseInt(currentId) - 1;
+      const newSquare = document.getElementById(newId);
+      click(newSquare);
+    }
+    // check UpLeft
+    if (!topEdge && !leftEdge) {
+      const newId = parseInt(currentId) - 1 - width;
+      const newSquare = document.getElementById(newId);
+      click(newSquare);
+    }
+    // check Up
+    if (!topEdge) {
+      const newId = parseInt(currentId) - width;
+      const newSquare = document.getElementById(newId);
+      click(newSquare);
+    }
+    // check UpRight
+    if (!topEdge && !rightEdge) {
+      const newId = parseInt(currentId) + 1 - width;
+      const newSquare = document.getElementById(newId);
+      click(newSquare);
+    }
+    // check Right
+    if (!rightEdge) {
+      const newId = parseInt(currentId) + 1;
+      const newSquare = document.getElementById(newId);
+      click(newSquare);
+    }
+    // check DownRight
+    if (!bottomEdge && !rightEdge) {
+      const newId = parseInt(currentId) + 1 + width;
+      const newSquare = document.getElementById(newId);
+      click(newSquare);
+    }
+    // check Down
+    if (!bottomEdge) {
+      const newId = parseInt(currentId) + width;
+      const newSquare = document.getElementById(newId);
+      click(newSquare);
+    }
+    // check DownLeft
+    if (!bottomEdge && ! leftEdge) {
+      const newId = parseInt(currentId) - 1 + width;
+      const newSquare = document.getElementById(newId);
+      click(newSquare);
+    }
+  }, 10);
 }
 
 createBoard();
